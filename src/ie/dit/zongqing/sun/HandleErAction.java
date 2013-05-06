f5611942a3c7cb77e2129626b5d3fb9e20943c8b
Zongqing <d09124243@mydit.ie>
2013-05-06 09:55:11 +0100

    Create Handle class. Start to design index.jsp

---
+++ src/ie/dit/zongqing/sun/HandleErAction.java
@@ -0,0 +1,29 @@
+package ie.dit.zongqing.sun;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.ServletActionContext;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HandleErAction extends ActionSupport {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public String execute() throws Exception{
+		HttpServletRequest req = ServletActionContext.getRequest();
+		HttpServletResponse res = ServletActionContext.getResponse();
+		
+		res.setContentType("text/xml;charset=UTF-8");
+		res.setHeader("Cache-Control", "no-cache");
+		
+		PrintWriter out = res.getWriter();
+		StringBuffer sb = new StringBuffer();
+		
+		ServletActionContext.getServletContext().setAttribute("app_flag", "false");
+		return "success";
+	}
+}
